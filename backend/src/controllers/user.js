5042dda2d74dddc710a25d537c11e569974b8d27
Anas Sain <98727007+AMS006@users.noreply.github.com>
2024-07-31 22:21:09 +0530

    Update user.js

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -4,16 +4,29 @@ const userModel = require("../schema/user");
 // User Login
 exports.signIn = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password, isGoogleLogin } = req.body;
 
-    const user = await userModel.findOne({ email });
+    let user = null;
+    if(isGoogleLogin){
+      user = await userModel.findOne({
+        email,
+        isGoogleLogin: true,
+      })
+      if(!user){
+        return res.status(400).json({message:"User not found"});
+      }
 
-    if (!user) return res.status(404).json({ message: "Invalid Credentials" });
+    }else{
+      user = await userModel.findOne({ email, isGoogleLogin: false });
+      if (!user) return res.status(404).json({ message: "Invalid Credentials" });
+  
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      if (!isPasswordCorrect)
+        return res.status(404).json({ message: "Invalid Credentials" });
+    }
 
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
-    if (!isPasswordCorrect)
-      return res.status(404).json({ message: "Invalid Credentials" });
+
 
     const token = user.generateJwtToken();
     return res.status(200).json({ user, token });
@@ -25,18 +38,24 @@ exports.signIn = async (req, res) => {
 // For Registration of New User
 exports.signUp = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const { name, email, password, isGoogleLogin } = req.body;
     const doesUserExists = await userModel.findOne({ email });
 
     if (doesUserExists)
       return res.status(400).json({ message: "User Already Exists" });
 
-    const user = await userModel.create({
-      name,
-      email,
-      password,
-    });
-
+    if(isGoogleLogin){
+      const user = await userModel.create({
+        name,
+        email,
+        isGoogleLogin: true
+      })
+      const token = user.generateJwtToken();
+      return res.status(201).json({ user, token });
+    }
+    
+    const user = await userModel.create({ name, email, password });
+    
     const token = user.generateJwtToken();
     return res.status(201).json({ user, token });
   } catch (error) {
